514cd361150ccb7d89bd3c1694d379be593421d6
Natnael Samuel <natisami901@gmail.com>
2025-03-30 13:05:10 +0300

    Added a responsive design

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
+
+function App() {
+  // Determine if the current screen size is "lg" or larger
+  const isLg = useBreakpointValue({ base: false, lg: true });
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show when={isLg}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
