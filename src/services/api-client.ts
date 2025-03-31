0102e6f92e3af3ea7ddbc131425a0dad5a28b1d1
Natnael Samuel <natisami901@gmail.com>
2025-03-31 13:05:00 +0300

    Show loading skeletons

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '1cd49277ceb14496bc0c1bd9eb6979bb'
+        key: '00181711387a4aa9bdc78e730bcba507'
     }
 })
