ef3efdcf2dd5894fd11e498381d6ffd426301da5
Natnael Samuel <natisami901@gmail.com>
2025-04-18 10:34:43 +0300

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,6 @@
 import useGenres, { Genre } from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { Box, HStack, Image, Link, Spinner } from "@chakra-ui/react";
+import { Box, Heading, HStack, Image, Link, Spinner } from "@chakra-ui/react";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
@@ -14,30 +14,36 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   if (isLoading) return <Spinner />;
 
   return (
-    <Box>
-      {data.map((genre) => (
-        <li key={genre.id} style={{ listStyle: "none", padding: "5px 0" }}>
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Link
-              fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
-              onClick={() => onSelectGenre(genre)}
-              fontSize="lg"
-              _hover={{
-                textDecoration: "underline",
-                textDecorationColor: "white",
-              }}
-            >
-              {genre.name}
-            </Link>
-          </HStack>
-        </li>
-      ))}
-    </Box>
+    <>
+      <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+      <Box>
+        {data.map((genre) => (
+          <li key={genre.id} style={{ listStyle: "none", padding: "5px 0" }}>
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Link
+                whiteSpace="normal"
+                textAlign="left"
+                fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                fontSize="lg"
+                _hover={{
+                  textDecoration: "underline",
+                  textDecorationColor: "white",
+                }}
+              >
+                {genre.name}
+              </Link>
+            </HStack>
+          </li>
+        ))}
+      </Box>
+    </>
   );
 };
 
